ad025824225acb342e8a6651eb798b6f0edbc752
sesco001 <marcasmatheka@gmail.com>
2025-10-08 15:06:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "https://backend12-jknk.onrender.com";
 
 export default function App() {
   const [q, setQ] = useState("");
